dbc3b59f45b2bbb303186d97d5651257d5ad440a
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2025-07-23 23:23:59 +0900

    useContextを実装した

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createContext, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const applemanInfo = {
+  name: "appleman",
+  age: 24,
+};
+
+const ApplemanContext = createContext(applemanInfo);
+
+createRoot(document.getElementById("root")).render(
+  <ApplemanContext.Provider value={applemanInfo}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ApplemanContext.Provider>
+);
+
+export default ApplemanContext;
