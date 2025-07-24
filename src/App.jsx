36af50aaf253fc6fd2c56d4be05100c702f01e23
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2025-07-24 14:50:40 +0900

    addし忘れたファイルを追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useEffect, useState, useContext, useRef } from "react";
+import { useEffect, useState, useContext, useRef, useReducer } from "react"; // 関数をReactライブラリからインポート
 import "./App.css";
-import ApplemanContext from "./main";
+import ApplemanContext from "./main"; // main.jsxからコンテンツをインポート
+
+// useReducerを定義する
+const reducer = (state, action) => {
+  switch (
+    action.type // アクションオブジェクトを定義
+  ) {
+    case "increment": // 増加
+      return state + 1;
+    case "decrement": // 減少
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-  const applemanInfo = useContext(ApplemanContext);
-  const ref = useRef();
+  const [count, setCount] = useState(0); // useStateを宣言
+  const applemanInfo = useContext(ApplemanContext); // インポートされたコンテキストの値を受け取る
+  const ref = useRef(); // useRefを宣言
+  const [state, dispatch] = useReducer(reducer, 0); // useReducerを宣言
 
   const handleClick = () => {
     setCount(count + 1);
   };
 
+  // useEffectを宣言
   useEffect(() => {
     console.log("Hello Hooks");
   }, [count]);
@@ -35,6 +51,12 @@ function App() {
       <h1>useRef</h1>
       <input type="text" ref={ref} />
       <button onClick={handleRef}>UseRef</button>
+
+      <hr />
+      <h1>useReducer</h1>
+      <p>カウント：{state}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
     </div>
   );
 }
